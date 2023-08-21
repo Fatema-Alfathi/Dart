e925d3f057f075666acf1384438db7e92ab0bba2
Fatema-Alfathi <132199582+Fatema-Alfathi@users.noreply.github.com>
2023-08-21 22:42:24 +0400

    Add files via upload

---
+++ app1/bin/app1.dart
@@ -0,0 +1,47 @@
+import 'dart:ffi';
+import 'dart:io';
+import 'dart:js_util';
+
+void main() {
+  // print and data type
+//   print(134);
+//   print("dart course");
+//   print(true);
+//   print(false);
+//   print(null);
+//   print('Dart');
+//   print(10+5);
+//   int a=10;
+
+// print by index
+// String c= 'sara';
+// print(c[0]);
+// print(c[1]);
+//   String e="hiiiiiiiiii";
+//   print(e);
+//   print(e[0]);
+//   print(e.length);
+
+// first way for date
+// // DateTime date=DateTime(2023,8,20);
+// // print(date);
+
+// Secand way of date
+// // DateTime datee=DateTime.now();
+// // print(datee);
+
+// like small if
+// // int ee =10;
+// // int d= 5;
+// // print(ee is double);
+
+// Null
+// // int? x;
+// print(x);
+//  x=5;
+
+// input
+//  print(10+x??0);
+//  print("Enter your name");
+//  int name= stdin.readByteSync();
+}
---
+++ app1/dart_application_1/bin/dart_application_1.dart
@@ -0,0 +1,27 @@
+void main() {
+  var list1 = [1, 2, 9];
+  // list1[0]=8;
+  // list1[1]=3;
+  // print(list1);
+
+  // list1.addAll([1,7,9]);
+  // list1.add(23);
+  // list1.remove(list1[0]);
+
+  // list1.removeAt(2);
+
+  // list1.removeLast;
+
+  // list1.removeRange(1,3);
+
+  // print(list1);
+  // final numbers = <int>[1, 2, 3, 5, 6, 7];
+  // var result = numbers.where((x) => x < 5); // (1, 2, 3)
+  // result = numbers.where((x) => x > 5); // (6, 7)
+  // result = numbers.where((x) => x.isEven); // (2, 6)
+  // print(result);
+
+  String name = "Hellow in dart";
+
+  print(name.replaceAll(' ', ''));
+}
