a7b4a95448b843f228aff51ec0a3c3cafa71fb33
Fatema-Alfathi <132199582+Fatema-Alfathi@users.noreply.github.com>
2023-08-23 12:46:09 +0400

    Add files via upload

---
+++ task7/bin/task7.dart
@@ -0,0 +1,63 @@
+import 'package:task7/task7.dart' as task7;
+
+void main() {
+  // mlty();
+  // mltyy();
+  // mltyyy(3, 2);
+  // c(4 * 4);
+
+  p(name: 'Fatema');
+  number();
+  n();
+  x();
+}
+// no return - no parameter
+// void printAnyThing() {
+//   print("Hellow");
+// }
+
+// // return - no paremetr
+// String getName() {
+//   return "Eslam";
+// }
+
+// // no return - paremetr
+// void setName(String name) {
+//   print(name);
+// }
+
+// int calc(int a, int b) {
+//   return (a + b);
+// }
+
+// void mlty() {
+//   print(2 * 4);
+// }
+
+// int mltyy() {
+//   return 1;
+// }
+
+// void mltyyy(int x, int y) {
+//   print(x * y);
+// }
+
+// int c(int e, int r) {
+//   return (4 * 4);
+// }
+
+void p({required String name}) {
+  print(name);
+}
+
+void number([String? n]) {
+  print(n);
+}
+
+void n({String? n}) {
+  print(n);
+}
+
+void x([String? n]) {
+  print(n);
+}
